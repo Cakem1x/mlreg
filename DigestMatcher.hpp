601dd9e21b211803c096616f995ff5c9bbd00a3b
Matthias Holoch <mholoch@gmail.com>
2014-12-29 18:12:25 +0100

    Added a new class for matching two digests.

---
+++ DigestMatcher.hpp
@@ -0,0 +1,21 @@
+//----------------------------------------------------------------------
+/*!\file
+ *
+ * \author  Matthias Holoch <mholoch@gmail.com>
+ * \date    2014-12-29
+ *
+ */
+//----------------------------------------------------------------------
+#ifndef DIGEST_MATCHER_HPP_INCLUDED
+#define DIGEST_MATCHER_HPP_INCLUDED
+
+#include "Digest.hpp"
+
+/*!
+ * The DigestMatcher is used to match two Digests.
+ * This means that this class uses the descriptors of the Digest to calculate a transformation between the two Digest's pointclouds when they overlap enough.
+ */
+class DigestMatcher {
+};
+
+#endif

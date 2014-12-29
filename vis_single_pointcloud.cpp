a6aa5825c154da6e56fc672dac4ccd8af04599f5
Matthias Holoch <mholoch@gmail.com>
2014-12-29 14:43:35 +0100

    added visualization of the cloud normals.

--- vis_single_pointcloud.cpp
+++ vis_single_pointcloud.cpp
@@ -12,8 +12,15 @@ int main(int argc, char** argv) {
   Digest digest(pointcloud, params);
   pcl::visualization::PCLVisualizer vis("Single Cloud Digest Visualization");
   vis.setBackgroundColor(0,0,0);
+
+  // Add the reduced pointcloud
   vis.addPointCloud<Digest::PointType>(digest.getReducedCloud(), "reduced_cloud");
   vis.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "reduced_cloud");
+
+  // Add the normals of the reduced pointcloud
+  vis.addPointCloudNormals<Digest::PointType, Digest::NormalType>(digest.getReducedCloud(), digest.getNormalCloud(), 10, 0.05, "reduced_cloud_normals");
+
+  // Add the keypoints with bigger dots
 
   while (!vis.wasStopped()) {
     vis.spinOnce(100);

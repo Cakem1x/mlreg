8ce9dae4c524615da71f91641417915b830dc21b
Matthias Holoch <mholoch@gmail.com>
2015-01-17 16:16:08 +0100

    Implemented transformation hints for the digest match.

--- DigestMatch.hpp
+++ DigestMatch.hpp
@@ -25,6 +25,17 @@ class DigestMatch {
       float hint_threshold_ = 0.9;
     };
 
+    struct TransformationHint {
+      typedef Eigen::Affine3f Transformation;
+
+      TransformationHint(Transformation transformation, float hint_confidence)
+        : transformation_(transformation), hint_confidence_(hint_confidence)
+      { }
+
+      Transformation transformation_;
+      float hint_confidence_;
+    };
+
     /*!
      * This struct defines the relation between a pair of descriptors of two pointclouds
      */
@@ -39,12 +50,17 @@ class DigestMatch {
     };
 
     typedef std::vector<Correspondence> Correspondences;
-    typedef Eigen::Affine3f Transformation;
+    typedef std::vector<struct TransformationHint> TransformationHints;
 
-    DigestMatch(std::shared_ptr<Digest> digest_source, std::shared_ptr<Digest> digest_target, struct Parameters &params)
+    /*!
+     * Most general Constructor.
+     * The machine learning module will be trained with each TransformationHint which has a big enough confidence value.
+     */
+    DigestMatch(std::shared_ptr<Digest> digest_source, std::shared_ptr<Digest> digest_target, struct Parameters &params, TransformationHints transformation_hints)
       : digest_source_(digest_source), 
         digest_target_(digest_target),
-        params_(params)
+        params_(params),
+        transformation_hints_(transformation_hints)
     {
       // Create a correspondence between each valid keypoint of the digests ("n²" correspondences)
       Digest::DescriptorCloud::Ptr descr_source = digest_source_->getDescriptorCloud();
@@ -54,6 +70,33 @@ class DigestMatch {
           correspondences_.push_back(Correspondence(i, j, descriptorDistance(descr_source->at(i), descr_target->at(i))));
         }
       }
+
+      //TODO: Do training (if possible)
+      
+      //TODO: Filter correspondences
+    };
+
+    /*!
+     * Constructor with a transformation_hint.
+     * The machine learning module will be trained with the transformation_hint
+     * when its confidence value is bigger than the threshold from the parameters.
+     */
+    DigestMatch(std::shared_ptr<Digest> digest_source, std::shared_ptr<Digest> digest_target, struct Parameters &params, TransformationHint& transformation_hint)
+      : DigestMatch(digest_source, digest_target, params, TransformationHints(1, transformation_hint))
+    { };
+
+    /*!
+     * Constructor without a transformation_hint.
+     * The machine learning module will not be trained.
+     */
+    DigestMatch(std::shared_ptr<Digest> digest_source, std::shared_ptr<Digest> digest_target, struct Parameters &params)
+      : DigestMatch(digest_source, digest_target, params, TransformationHints())
+    { };
+
+    /*!
+     * Destructor.
+     */
+    virtual ~DigestMatch() {
     };
 
   protected:
@@ -61,7 +104,7 @@ class DigestMatch {
     std::shared_ptr<Digest> digest_target_;
     Correspondences correspondences_;
     struct Parameters params_;
-    Transformation transformation_hint_;
+    TransformationHints transformation_hints_;
 
     /*!
      * Returns the distance of two FPFHSignatur33 descriptors.

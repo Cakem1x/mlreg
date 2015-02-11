f5f0a3dda41e1416ab1020e1259e536d0192933d
Matthias Holoch <mholoch@gmail.com>
2015-02-11 11:11:33 +0100

    Fixed the way I used OpenCV's SVM.

--- MLMSVM.hpp
+++ MLMSVM.hpp
@@ -52,24 +52,23 @@ class MLMSVM : public MLModule {
       // Stores the transformed target cloud
       Digest::Cloud::Ptr cloud_target(new Digest::Cloud);
       // Stores the training data
-      cv::Mat trainingData(33, correspondences.size(), CV_32FC1);
+      cv::Mat trainingData(correspondences.size(), 33, CV_32FC1);
       // Stores the training labels
-      cv::Mat labels(1, correspondences.size(), CV_32SC1);
+      cv::Mat labels(correspondences.size(), 1, CV_32SC1);
 
       // Transform the target cloud with the tf from the TransformationHint:
       pcl::transformPointCloud(*(digest_target->getReducedCloud()), *cloud_target, transformation_hint.transformation);
 
       // Iterate through all correspondences and define their class by the distance between the transformed corresponding points
       for (unsigned int i = 0; i < correspondences.size(); ++i) {
-        bool correct_correspondence;
         Digest::PointType p_src = cloud_source->at(digest_source->getDescriptorCloudIndices()->at(correspondences[i].source_id));
         Digest::PointType p_trg = cloud_target->at(digest_target->getDescriptorCloudIndices()->at(correspondences[i].target_id));
         Digest::PointType p_diff(p_src.x - p_trg.x, p_src.y - p_trg.y, p_src.z - p_trg.z);
         // set the class of the correspondence depending on their squared euclidean distance
-        labels.at<int>(1,i) = (p_diff.x * p_diff.x + p_diff.y * p_diff.y + p_diff.z * p_diff.z <= params_.correct_corr_max_distance_squared);
+        labels.at<int>(i,1) = (p_diff.x * p_diff.x + p_diff.y * p_diff.y + p_diff.z * p_diff.z <= params_.correct_corr_max_distance_squared);
         // add the feature vector to the training data
         for (unsigned int j = 0; j < 33; ++j) {
-          trainingData.at<float>(j,i) = correspondences[i].distance.histogram[j];
+          trainingData.at<float>(i,j) = correspondences[i].distance.histogram[j];
         }
       }
       // train the svm
